ce9364281495af065faf5af2ca7a2ea63e5c8df4
ali attarian <ali.live26.aa@gmail.com>
2023-04-05 14:46:04 +0330

    feat: member model added

--- members/models.py
+++ members/models.py
@@ -6,5 +6,13 @@ from django.conf import settings
 class User(AbstractUser):
     email = models.EmailField(unique=True)
 
+def member_photo_path(instance, filename):
+    return 'members/photos/{0}'.format(instance.user.username)
+
 class Member(models.Model):
+    photo = models.ImageField(upload_to=member_photo_path, null=True)
+    phone = models.CharField(max_length=255)
+    bio = models.TextField(null=True)
+    birth_date = models.DateField()
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
--- nitro_back/settings.py
+++ nitro_back/settings.py
@@ -128,3 +128,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'members.User'
+
+MEDIA_URL = ''
+MEDIA_ROOT = ''
