dd58614887c2fd8199e915716ec5d11363d173de
ali attarian <ali.live26.aa@gmail.com>
2023-04-08 17:28:43 +0330

    feat: debug_toolbar fixed

--- nitro_back/settings.py
+++ nitro_back/settings.py
@@ -131,6 +131,12 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'members.User'
