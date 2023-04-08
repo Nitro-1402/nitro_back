0ee09917c8847d4d51c004640c6e1a49de460011
ali attarian <ali.live26.aa@gmail.com>
2023-04-08 17:37:28 +0330

    feat: news queryset improved

--- movies/views.py
+++ movies/views.py
@@ -4,5 +4,6 @@ from .models import *
 from .serializers import *
 
 class NewsViewSet(ModelViewSet):
-    queryset = News.objects.all()
+    queryset = News.objects.prefetch_related(
+        'movies').prefetch_related('actors').prefetch_related('directors').all()
     serializer_class = NewsSerializer
