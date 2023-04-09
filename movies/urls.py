09f48a570c68b7238890101e37b7799f506070db
ali attarian <ali.live26.aa@gmail.com>
2023-04-09 11:54:55 +0330

    feat: swagger ui created

--- movies/urls.py
+++ movies/urls.py
@@ -1,7 +1,13 @@
+from django.urls import path
 from rest_framework_nested import routers
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 router = routers.DefaultRouter()
 router.register('news', views.NewsViewSet, basename='news')
 
-urlpatterns = router.urls
+urlpatterns_schema = [
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
+]
+urlpatterns = router.urls + urlpatterns_schema
