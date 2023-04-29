ce8ed1537cbe346732ae6a7fc3482956193f7922
ali attarian <ali.live26.aa@gmail.com>
2023-04-29 16:48:59 +0330

    fix: urls fixed

--- members/urls.py
+++ members/urls.py
@@ -1,14 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from rest_framework_nested import routers
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from . import views
 
 router = routers.DefaultRouter()
 router.register('profile', views.ProfileViewSet, basename='profile')
-router.register('followers', views.FollowersListViewSet, basename='followers')
 
 # urlpatterns_schema = [
 #     path('schema/', SpectacularAPIView.as_view(), name='schema'),
 #     path('schema/docs/', SpectacularSwaggerView.as_view(url_name='schema')),
 # ]
-urlpatterns = router.urls
+urlpatterns = [
+    path('', include(router.urls)),
+    path('followers', views.FollowersListViewSet.as_view(), basename='followers')
+]
