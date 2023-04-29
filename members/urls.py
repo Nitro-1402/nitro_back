90b3430690ec9e1656774fc35a375a4c9e96c5c1
ali attarian <ali.live26.aa@gmail.com>
2023-04-29 16:52:02 +0330

    fix: urls fixed again

--- members/urls.py
+++ members/urls.py
@@ -12,5 +12,5 @@ router.register('profile', views.ProfileViewSet, basename='profile')
 # ]
 urlpatterns = [
     path('', include(router.urls)),
-    path('followers', views.FollowersListViewSet.as_view(), basename='followers')
+    path('followers', views.FollowersListViewSet.as_view(), name='followers')
 ]
