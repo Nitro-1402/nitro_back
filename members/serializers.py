e37c5cd5e5f43c4a584f131ae054b2de79bcaeec
ali attarian <ali.live26.aa@gmail.com>
2023-04-29 17:08:29 +0330

    feat: user create updated

--- members/serializers.py
+++ members/serializers.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError, transaction
 from djoser.serializers import UserCreateSerializer as BaseUserSerializer
 from rest_framework import serializers
 from .models import *
@@ -11,6 +12,23 @@ class UserCreateSerializer(BaseUserSerializer):
     
     class Meta(BaseUserSerializer.Meta):
         fields = ['id', 'username', 'password', 'email']
+
+    def create(self, validated_data):
+        try:
+            user = self.perform_create(validated_data)
+        except IntegrityError:
+            self.fail("cannot_create_user")
+
+        return user
+
+    def perform_create(self, validated_data):
+        with transaction.atomic():
+            user = User.objects.create_user(**validated_data)
+            profile = Profile.objects.create(user_id=user.id)
+            if settings.SEND_ACTIVATION_EMAIL:
+                user.is_active = False
+                user.save(update_fields=["is_active"])
+        return user
 
 class SimpleProfileSerializer(serializers.ModelSerializer):
     class Meta:
