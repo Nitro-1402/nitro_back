0a78ac6034818acd304e25f68fef6412ae50e444
ali attarian <ali.live26.aa@gmail.com>
2023-05-12 05:23:24 +0330

    feat: change some permissions

--- members/permissions.py
+++ members/permissions.py
@@ -6,14 +6,10 @@ class IsSubscriber(permissions.BasePermission):
         if request.user:
             if request.user.is_staff:
                 return True
-            print(str(request.user.id))
-            subscribed_to_list = Profile.objects.filter(user_id=request.user.id).values_list('subscribed_to')
-            return bool(Profile.objects.filter(
-                id=view.kwargs['profile_pk']).filter(subscribers__in=subscribed_to_list).exists())
-            print("subscribed to" + str(subscribed_to_list))
-            is_subscribed = False
-            print("pofile_pk" + str(view.kwargs['profile_pk']))
-            if view.kwargs['profile_pk'] in subscribed_to_list:
-                is_subscribed = True
-            return bool(is_subscribed)
+            if request.user.profile.id == view.kwargs['profile_pk']:
+                return True
+            if request.method in permissions.SAFE_METHODS:
+                subscribed_to_list = Profile.objects.filter(user_id=request.user.id).values_list('subscribed_to')
+                return bool(Profile.objects.filter(
+                    id=view.kwargs['profile_pk']).filter(subscribers__in=subscribed_to_list).exists())
         return False
