011f7c4228408f85668864d8ab70fcc1a3de91e0
ali attarian <ali.live26.aa@gmail.com>
2023-05-19 02:43:32 +0330

    feat: LikeCommentPermission

--- comments/permissions.py
+++ comments/permissions.py
@@ -12,9 +12,18 @@ class CommentPermission(permissions.BasePermission):
                 return True
             if Profile.objects.filter(id=request.user.profile.id,comment__in=view.kwargs['pk']).exists():
                 return True
-            if request.method in permissions.SAFE_METHODS:
-                subscribed_to_list = Profile.objects.filter(user_id=request.user.id).values_list('subscribed_to')
-                return bool(Profile.objects.filter(
-                    id=view.kwargs['profile_pk']).filter(subscribers__in=subscribed_to_list).exists())
+        else:
+            return False
+        
+class LikeCommentPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        elif request.user.is_authenticated:
+            if request.user.is_staff:
+                return True
+            if request.method == 'POST':
+                if request.user.profile.id == request.data.get('profile'):
+                    return True
         else:
             return False
