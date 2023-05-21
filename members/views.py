4890b6da16617ceeade7cfd949f3032716a4fa0c
ali attarian <ali.live26.aa@gmail.com>
2023-05-21 23:30:57 +0330

    fix: user to profile in delete subscribe

--- members/views.py
+++ members/views.py
@@ -138,8 +138,8 @@ class AddSubscriberViewSet(mixins.CreateModelMixin,GenericViewSet):
 
     @action(detail=False, methods=['DELETE'])
     def unsubscribe(self, request):
-        user_id = request.GET.get('user_id')
+        profile_id = request.GET.get('user_id')
         subscriber_id = request.GET.get('subscriber_id')
-        subscribe = get_object_or_404(Subscribe, user_id=user_id, subscriber_id=subscriber_id)
+        subscribe = get_object_or_404(Subscribe, profile_id=profile_id, subscriber_id=subscriber_id)
         subscribe.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
