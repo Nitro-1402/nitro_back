fd6d4664a55364caa38c165207e59d9203cc0b3d
ali attarian <ali.live26.aa@gmail.com>
2023-05-21 20:37:07 +0330

    feat: permission for views in movies app

--- movies/views.py
+++ movies/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import *
 from .models import *
 from .serializers import *
 from .pagination import *
@@ -10,34 +11,105 @@ class NewsViewSet(ModelViewSet):
     serializer_class = NewsSerializer
     pagination_class = NewsPagination
 
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
+
 class ActorViewSet(ModelViewSet):
     queryset = Actor.objects.all()
     serializer_class = ActorSerializer
-    authentication_classes = []
+
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
 
 class CategoryViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     pagination_class = None
 
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
+
 class MovieViewSet(ModelViewSet):
     queryset = Movie.objects.select_related('director').prefetch_related('actors').prefetch_related('category_set').all()
     serializer_class = MovieSerializer
     pagination_class = MoviePagination
+
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
 
 
 class DirctorViewSet(ModelViewSet):
     queryset = Director.objects.all()
     serializer_class = DirectorSerializer
 
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
+
 class SeasonViewSet(ModelViewSet):
     queryset = Series_season.objects.select_related('series').all()
     serializer_class = SeasonSerializer
+
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
 
 class EpisodeViewSet(ModelViewSet):
     queryset = Series_episode.objects.select_related('season').all()
     serializer_class = EpisodeSerializer
 
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
+
 class RatingViewSet(ModelViewSet):
     queryset = Rating.objects.select_related('user').select_related('movie').all()
     serializer_class = RatingSerializer
+
+    def get_authenticators(self):
+        if self.request is not None:
+            if self.request.method in SAFE_METHODS:
+                return []  
+            else:
+                return super().get_authenticators()
+        else:
+            return super().get_authenticators()
