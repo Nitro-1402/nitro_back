642cd2d0949a1b6354f30cd56e58d84cfa4a6aea
محمد امین رضاپور <m_rezapour@comp.iust.ac.ir>
2023-05-23 09:09:48 +0430

    set email settings

--- nitro_back/settings.py
+++ nitro_back/settings.py
@@ -192,3 +192,10 @@ DJOSER = {
 }
 
 SEND_ACTIVATION_EMAIL = False
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "localhost"
+EMAIL_PORT = "1025"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
