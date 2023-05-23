c3bcb0c2e4c6d2eadaafba0dcd3a1e21d480b589
محمد امین رضاپور <m_rezapour@comp.iust.ac.ir>
2023-05-23 15:36:41 +0430

    fix : set password for email

--- nitro_back/settings.py
+++ nitro_back/settings.py
@@ -197,4 +197,4 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'nitroback007@gmail.com'
-EMAIL_HOST_PASSWORD = 'nitro.back'
+EMAIL_HOST_PASSWORD = 'itvcbyjyrxtycumr'
